ba40809f28d7f35a38d55c3fd2b49a1f349f9761
Fangbiao Zhang(Outsourcing) <fangbiao.zhang@energy.envision.com>
2021-06-10 17:28:41 +0800

    pull

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -15,7 +15,8 @@ else
   git remote set-url origin "$REPO_URL"
   git reset --hard
   echo "git pull拉取最新代码..."
-  git -C /scripts pull --rebase
+  echo "$REPO_URL"
+  git pull --rebase
   echo "npm install 安装最新依赖"
   npm install --prefix /scripts
 fi
